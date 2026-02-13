da921484c0bf52c91765a50a358e00a4a601a5b5
Tim <60017293+tim-1108@users.noreply.github.com>
2026-02-13 21:23:24 +0100

    Quick hack to pretend that any dimension in the "Hauptwelt" is just `minecraft:overworld` instead of `minecraft:main_xx`

--- src/main/java/me/cortex/voxy/commonImpl/mixin/minecraft/MixinWorld.java
+++ src/main/java/me/cortex/voxy/commonImpl/mixin/minecraft/MixinWorld.java
@@ -1,9 +1,12 @@
 package me.cortex.voxy.commonImpl.mixin.minecraft;
 
+import me.cortex.voxy.common.Logger;
 import me.cortex.voxy.commonImpl.IWorldGetIdentifier;
 import me.cortex.voxy.commonImpl.WorldIdentifier;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
@@ -14,10 +17,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
+
 @Mixin(Level.class)
 public class MixinWorld implements IWorldGetIdentifier {
     @Unique
     private WorldIdentifier identifier;
+    @Unique
+    private final String[] overworldNames = {"main_sw", "main_nw", "main_no"};
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void voxy$injectIdentifier(WritableLevelData properties,
@@ -30,6 +37,13 @@ public class MixinWorld implements IWorldGetIdentifier {
                                        int maxChainedNeighborUpdates,
                                        CallbackInfo ci) {
         if (key != null) {
+            // Path will be "overworld", "the_nether", or our desired names we want to overwrite!
+            String path = key.identifier().getPath();
+            Logger.info("Entered dimension with path " + path);
+            if (Arrays.asList(overworldNames).contains(path)) {
+                Logger.info("Dimension has matched, replacing with overworld");
+                key = ResourceKey.create(Registries.DIMENSION, Identifier.fromNamespaceAndPath("minecraft", "overworld"));
+            }
             this.identifier = new WorldIdentifier(key, seed, dimensionEntry == null?null:dimensionEntry.unwrapKey().orElse(null));
         } else {
             this.identifier = null;
